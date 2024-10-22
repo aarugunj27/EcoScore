5113ff9913a5c95cb57a5e91b8c50c9bb1e6cb3a
Aarush Gunjal <aarugunj27@ncs.charter.k12.de.us>
2024-10-22 16:20:38 -0400

    updates

---
+++ client/src/pages/TrackerPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import EcoScoreCalculator from '../components/EcoScoreCalculator';
+
+function TrackerPage() {
+  return (
+    <EcoScoreCalculator/>
+  );
+}
+
+export default TrackerPage;
